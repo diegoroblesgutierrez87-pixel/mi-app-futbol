b44056eb76d20ff9dd0537ff3da2aed1f86e16de
diegoroblesgutierrez@gmail.com <diegoroblesgutierrez87@gmail.com>
2026-09-07 17:26:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -315,14 +315,7 @@ if filtro_tipo!= "Ninguno":
         st.warning(f"⚠️ {liga_sel} no tiene amarillas"); hay_datos=False
 
     if hay_datos:
-        if eq1!= "Ninguno" and eq2!= "Ninguno":
-            equipos_a_chequear = [eq1, eq2]
-        elif eq1!= "Ninguno":
-            equipos_a_chequear = 
-        elif eq2!= "Ninguno":
-            equipos_a_chequear = 
-        else:
-            equipos_a_chequear = equipos if liga_sel!="Todas" else sorted(pd.unique(pd.concat([df['HomeTeam'], df['AwayTeam']]).dropna()).tolist())
+        equipos_a_chequear = equipos if liga_sel!="Todas" else sorted(pd.unique(pd.concat([df['HomeTeam'], df['AwayTeam']]).dropna()).tolist())
         calificados = []
         for team in equipos_a_chequear:
             d_team = df_f[(df_f['HomeTeam']==team)|(df_f['AwayTeam']==team)]
