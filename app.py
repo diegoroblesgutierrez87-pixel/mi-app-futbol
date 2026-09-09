527e0f609147dd8d49b06507bf25cd8e998768ff
diegoroblesgutierrez@gmail.com <diegoroblesgutierrez87@gmail.com>
2026-09-09 16:52:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -337,7 +337,11 @@ def fmt_rapido(r, eq_refs_norm, current_eq_norm, current_eq_orig):
                 txt_extra = f'({abbr}){alterador}'
 
             team_full = r.get('HomeTeam','') if team == h_norm else r.get('AwayTeam','') if team == a_norm else abbr
-            html_gol = f'<span style="color:#000; display:inline-block; margin:2px 0">{team_full} | {m}\' |{alterador}</span>'
+            if mj == "ON" and gol_ab:
+                extra_jug = f' "{gol_ab}"' + (f' ({asi_ab})' if asi_ab else '')
+                html_gol = f'<span style="color:#000; display:inline-block; margin:2px 0">{team_full} | {m}\' |{alterador}{extra_jug}</span>'
+            else:
+                html_gol = f'<span style="color:#000; display:inline-block; margin:2px 0">{team_full} | {m}\' |{alterador}</span>'
             if m <= 45:
                 mins_1t.append(html_gol)
             else:
