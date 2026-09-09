d66a88cfaa7ae1391248d1991fd2dd7ada649cab
diegoroblesgutierrez@gmail.com <diegoroblesgutierrez87@gmail.com>
2026-09-09 18:30:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -337,11 +337,13 @@ def fmt_rapido(r, eq_refs_norm, current_eq_norm, current_eq_orig):
                 txt_extra = f'({abbr}){alterador}'
 
             team_full = r.get('HomeTeam','') if team == h_norm else r.get('AwayTeam','') if team == a_norm else abbr
+            es_mio = any(ern in team or team in ern for ern in eq_refs_norm) if eq_refs_norm else False
+            color_gol = "#9370DB" if es_mio else "#000"
             if mj == "ON" and gol_ab:
                 extra_jug = f' "{gol_ab}"' + (f' ({asi_ab})' if asi_ab else '')
-                html_gol = f'<span style="color:#000; display:inline-block; margin:2px 0">{team_full} | {m}\' |{alterador}{extra_jug}</span>'
+                html_gol = f'<span style="color:{color_gol}; display:inline-block; margin:2px 0; font-weight:900">{team_full} | {m}\' |{alterador}{extra_jug}</span>' if es_mio else f'<span style="color:{color_gol}; display:inline-block; margin:2px 0">{team_full} | {m}\' |{alterador}{extra_jug}</span>'
             else:
-                html_gol = f'<span style="color:#000; display:inline-block; margin:2px 0">{team_full} | {m}\' |{alterador}</span>'
+                html_gol = f'<span style="color:{color_gol}; display:inline-block; margin:2px 0; font-weight:900">{team_full} | {m}\' |{alterador}</span>' if es_mio else f'<span style="color:{color_gol}; display:inline-block; margin:2px 0">{team_full} | {m}\' |{alterador}</span>'
             if m <= 45:
                 mins_1t.append(html_gol)
             else:
