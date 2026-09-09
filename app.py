3e71f67eade19cb28d3ab8afa9cf32d0c7750a0c
diegoroblesgutierrez@gmail.com <diegoroblesgutierrez87@gmail.com>
2026-09-09 15:12:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -406,7 +406,7 @@ def fmt_rapido(r, eq_refs_norm, current_eq_norm, current_eq_orig):
                     extra += f" <span style='color:#888'>[{tot_f}f]</span>"
     except: pass
 
-    # G/E/P del equipo seleccionado en 1P y 2P
+    # G/E/P del equipo seleccionado: 1P / FINAL
     def res_eq(gf, gc):
         if gf > gc: return "G"
         if gf == gc: return "E"
@@ -417,14 +417,13 @@ def fmt_rapido(r, eq_refs_norm, current_eq_norm, current_eq_orig):
 
     if es_local:
         r1 = res_eq(hthg, htag)
-        r2 = res_eq(hg - hthg, ag - htag)
-        btts_txt = f"{r1}/{r2}"
+        rF = res_eq(hg, ag)
+        btts_txt = f"{r1}/{rF}"
     elif es_visit:
         r1 = res_eq(htag, hthg)
-        r2 = res_eq(ag - htag, hg - hthg)
-        btts_txt = f"{r1}/{r2}"
+        rF = res_eq(ag, hg)
+        btts_txt = f"{r1}/{rF}"
     else:
-        # sin equipo seleccionado -> dejamos G/G = BTTS
         btts = hg > 0 and ag > 0
         btts_txt = "G/G" if btts else "NG/NG"
 
