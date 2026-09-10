da2d9ad0a919b82c07058bd90770a26b3edc292a
diegoroblesgutierrez@gmail.com <diegoroblesgutierrez87@gmail.com>
2026-09-10 04:31:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -468,6 +468,7 @@ def fmt_rapido(r, eq_refs_norm, current_eq_norm, current_eq_orig):
             hst = _iv(r.get('HST')); ast = _iv(r.get('AST'))
             hs = _iv(r.get('HS')); _as = _iv(r.get('AS'))
             hf = _iv(r.get('HF')); af = _iv(r.get('AF'))
+            hsv = _iv(r.get('HomeSaves')); asv = _iv(r.get('AwaySaves'))
 
             hp = _iv(r.get('HomePasses')); ap = _iv(r.get('AwayPasses'))
             hpos = _iv(r.get('HomePos')); apos = _iv(r.get('AwayPos'))
@@ -484,6 +485,8 @@ def fmt_rapido(r, eq_refs_norm, current_eq_norm, current_eq_orig):
                 extra += f" <span style='color:#004080'>[HS{hs if hs is not None else '-'}]</span> <span style='color:#5A8AC0'>[AS{_as if _as is not None else '-'}]</span>"
             if hf is not None or af is not None:
                 extra += f" <span style='color:#666'>[HF{hf if hf is not None else '-'}]</span> <span style='color:#999'>[AF{af if af is not None else '-'}]</span>"
+            if hsv is not None or asv is not None:
+                extra += f" <span style='color:#008080'>[HSV{hsv if hsv is not None else '-'}]</span> <span style='color:#20B2AA'>[ASV{asv if asv is not None else '-'}]</span>"
             if hp is not None or ap is not None:
                 extra += f" <span style='color:#2E8B57'>[HP{hp if hp is not None else '-'}]</span> <span style='color:#3CB371'>[AP{ap if ap is not None else '-'}]</span>"
             if hpos is not None or apos is not None:
